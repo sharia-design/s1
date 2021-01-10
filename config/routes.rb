fe23175fcba584c3c393d97b765159fba136b700
Saba <shariasaba@gmail.com>
2021-01-11 00:04:33 +0400

    Fuck it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :cars
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For Thank details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
