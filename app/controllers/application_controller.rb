0f9a2b483ca784047a0576b67280259745c518c4
Saba <shariasaba@gmail.com>
2021-01-13 01:31:37 +0400

    updating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    def index
+        @_testData = "Saba"
+        render "layouts/index"
+
+    end
+
+    def test
+        render "layouts/test"
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root to: "application#index"
+
+  get '/test', to: 'application#test'
+
   resources :cars
-  # For Thank details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+
+
+
+
+
+
+  
 end
